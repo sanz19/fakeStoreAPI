3554d5756f1cfac84fa06497cff5a3a8cb947bad
sanz19 <ra.sanz19@info.uas.edu.mx>
2022-12-08 19:38:04 -0700

    primero2

--- src/app/productsapi.service.ts
+++ src/app/productsapi.service.ts
@@ -9,7 +9,7 @@ export class ProductsapiService {
   constructor(private http: HttpClient) {
 
    }
-
+   //hola
    obtenerProductos(){
     return this.http.get('https://fakestoreapi.com/products');
    }
